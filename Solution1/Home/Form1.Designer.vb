7749c264ae10ff143551750addce5b97fc414d05
jocastro92 <73299708+jocastro92@users.noreply.github.com>
2021-01-29 16:40:39 -0600

    Primer Commit del proyecto

---
+++ Solution1/Home/Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Me.LabelBienvenida = New System.Windows.Forms.Label()
+		Me.SuspendLayout()
+		'
+		'LabelBienvenida
+		'
+		Me.LabelBienvenida.AutoSize = True
+		Me.LabelBienvenida.Font = New System.Drawing.Font("Arial Black", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+		Me.LabelBienvenida.Location = New System.Drawing.Point(258, 193)
+		Me.LabelBienvenida.Name = "LabelBienvenida"
+		Me.LabelBienvenida.Size = New System.Drawing.Size(245, 45)
+		Me.LabelBienvenida.TabIndex = 0
+		Me.LabelBienvenida.Text = "Proyecto IG2"
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(800, 450)
+		Me.Controls.Add(Me.LabelBienvenida)
+		Me.Name = "Form1"
+		Me.Text = "Form1"
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+
+	Friend WithEvents LabelBienvenida As Label
+End Class
---
+++ Solution1/Home/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+	Private Sub Label1_Click(sender As Object, e As EventArgs) Handles LabelBienvenida.Click
+
+	End Sub
+End Class
