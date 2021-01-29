2c9e0d763e3627581133a61d32916ac138df1108
jocastro92 <73299708+jocastro92@users.noreply.github.com>
2021-01-29 16:43:37 -0600

    Segundo Commit del proyecto

--- Solution1/Home/Form1.Designer.vb
+++ Solution1/Home/Form1.Designer.vb
@@ -23,23 +23,35 @@ Partial Class Form1
 	<System.Diagnostics.DebuggerStepThrough()> _
 	Private Sub InitializeComponent()
 		Me.LabelBienvenida = New System.Windows.Forms.Label()
+		Me.Label1 = New System.Windows.Forms.Label()
 		Me.SuspendLayout()
 		'
 		'LabelBienvenida
 		'
 		Me.LabelBienvenida.AutoSize = True
 		Me.LabelBienvenida.Font = New System.Drawing.Font("Arial Black", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-		Me.LabelBienvenida.Location = New System.Drawing.Point(258, 193)
+		Me.LabelBienvenida.Location = New System.Drawing.Point(88, 67)
 		Me.LabelBienvenida.Name = "LabelBienvenida"
 		Me.LabelBienvenida.Size = New System.Drawing.Size(245, 45)
 		Me.LabelBienvenida.TabIndex = 0
 		Me.LabelBienvenida.Text = "Proyecto IG2"
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Font = New System.Drawing.Font("Arial Black", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+		Me.Label1.Location = New System.Drawing.Point(385, 236)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(245, 45)
+		Me.Label1.TabIndex = 1
+		Me.Label1.Text = "Proyecto IG2"
 		'
 		'Form1
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
 		Me.ClientSize = New System.Drawing.Size(800, 450)
+		Me.Controls.Add(Me.Label1)
 		Me.Controls.Add(Me.LabelBienvenida)
 		Me.Name = "Form1"
 		Me.Text = "Form1"
@@ -49,4 +61,5 @@ Partial Class Form1
 	End Sub
 
 	Friend WithEvents LabelBienvenida As Label
+	Friend WithEvents Label1 As Label
 End Class
